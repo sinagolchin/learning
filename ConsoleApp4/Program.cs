151990925bda347a70c3b14473d849653bf3eea9
sinagolchin <47105729+sinagolchin@users.noreply.github.com>
2019-02-11 14:00:55 +0330

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            int x;
+            x = 98;
+            Console.WriteLine(x);
+        }
+    }
+}
